da67f4e2d1ab2e74024bfbac14706882ce161c2e
RickFlist <theosomos@gmail.com>
2017-06-01 13:30:12 -0700

    asl;kjdf\

--- Scratch/WindowsLibraries.ps1
+++ Scratch/WindowsLibraries.ps1
@@ -1,3 +1,7 @@
+<#
+Inspiration: https://www.petri.com/creating-custom-xml-in-powershell
+#>
+
 $libMsLocation = [System.IO.DirectoryInfo] 'C:\Users\RickFlist\AppData\Roaming\Microsoft\Windows\Libraries'
 
 [Xml]$Doc = New-Object -TypeName System.Xml.XmlDocument
@@ -5,8 +9,9 @@ $dec = $doc.CreateXmlDeclaration("1.0","UTF-8",$null)
 $Doc.AppendChild($dec)
 
 $newXmlNs = $Doc.CreateElement("libraryDescription","xmlns","http://schemas.microsoft.com/windows/2009/library")
-
 $Doc.AppendChild($newXmlNs)
+
+$doc.CreateNode([System.Xml.XmlNo, "libraryDescription","http://schemas.microsoft.com/windows/2009/library")
 
 $Doc.Normalize()
 $Doc.Save('C:\Temp\temp.xml')
